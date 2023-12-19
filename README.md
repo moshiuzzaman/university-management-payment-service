72abb0c88f094a1c272714294f5331301ad912ec
moshiuzzaman <md.shozon041@gmail.com>
2023-12-19 11:02:18 +0600

    feat: done payment service

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+  <h1>University Management System</h1>
+</div>
+<div align="center">
+  <strong>Payment Service</strong>
+</div>
+
---
+++ src/app/modules/payment/payment.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { PaymentController } from './payment.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+
+const router = express.Router();
+
+router.get('/', auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), PaymentController.getAllFromDB);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), PaymentController.getByIdFromDB);
+
+router.post('/init', PaymentController.initPayment)
+
+router.post('/webhook', PaymentController.webhook)
+
+export const paymentRoutes = router;
+
+
